5c16a51d1a1fe2452f7d7e0d0bffafb8ad799e47
Spoxbz <minerolgestbz@gmail.com>
2024-05-22 11:57:33 -0500

    cmvoid

--- Dia3/EjemploAtaque.py
+++ Dia3/EjemploAtaque.py
@@ -1,3 +1,4 @@
+"""
 import zipfile
 
 passw = "pass1"
@@ -6,3 +7,28 @@ try:
     zfile.extractall(pwd=passw.encode('utf-8'))
 except Exception as error:
     print(error)
+"""
+
+import zipfile
+
+def extract_file(zfile, password):
+    try:
+        zfile.extractall(pwd=password.encode('utf-8'))
+        return password
+    except Exception as error:
+        print(error)
+
+def main():
+    zfile = zipfile.ZipFile('data.zip')
+    pass_file = open('dictionary.txt')
+
+    for line in pass_file.readlines():
+        password = line.strip("\n")
+        guess = extract_file(zfile=zfile, password=password)
+        
+        if guess:
+            print(f"[+] Password {password} \n")
+            exit(0)
+
+if __name__ == '__main__':
+    main()
